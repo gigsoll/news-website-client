4993739268b4fb54befb0fc7323ec1f824438866
gigsoll <gigsollt@gmail.com>
2024-03-31 15:30:34 +0300

    reorganized src folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
+import Main from './scripts/Main';
+import reportWebVitals from './scripts/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
---
+++ src/scripts/Main.js
@@ -0,0 +1,12 @@
+import '../styles/Main.css';
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default Main;
